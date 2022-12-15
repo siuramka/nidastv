854b024c2d7e70eb3aa47b71b186666e279a928e
Marius Kamarauskas <mariusultimate@gmail.com>
2022-12-15 16:29:34 +0200

    Time for chat later

--- milfend/src/pages/StreamVod.js
+++ milfend/src/pages/StreamVod.js
@@ -1,30 +1,38 @@
 import React from 'react'
-import { useParams } from 'react-router-dom';
-import { useEffect, useRef } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
 import Hls from 'hls.js'
-export function StreamVod() {
-  const {path} = useParams();
-  const mounted = useRef(false);
-  let url = "https://cloud.nidas.tv/hls/"+ path + ".av1" +"/output.m3u8"
-  useEffect(() => {
-    mounted.current = true;
-      if (Hls.isSupported()) {
-        const hls = new Hls();
-        console.log(url)
-        const video = document.getElementById('my-video');
-        hls.loadSource(url);
-        hls.attachMedia(video);
-        hls.on(Hls.Events.MANIFEST_PARSED, () => {
-          video.play();
-        });
-      }
-    return () => {
-        mounted.current = false;
-    };
-}, []);
 
-  return mounted ? (
-    <video id="my-video" controls />
-  ):(null);
+export function StreamVod() {
+  const { path } = useParams();
+  const [error, setError] = useState(true)
+  const[currentTime1, setCurrentTime1] = useState('00:00');
+  const videoRef = useRef();
+  let url = "https://cloud.nidas.tv/hls/" + path + ".av1" + "/output.m3u8"
+
+
+  const timeUpdate = (event) => {
+    const currentTime = event.target.currentTime
+    console.log(currentTime);
+    setCurrentTime1(currentTime);
+}
+  var loadVid = function () {
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.attachMedia(videoRef.current);
+      hls.once(Hls.Events.MEDIA_ATTACHED, (event, data) => {
+        hls.loadSource(url);
+      })
+      hls.on(Hls.Events.ERROR, function (event, data) {
+        setError(false)
+      });
+    }
+  }
+
+  useEffect(() => {
+    loadVid()
+  }, []);
+
+  return error ? (<video ref={videoRef} width={1000} onTimeUpdate={(event) => {timeUpdate(event)} } controls />) : (<>wwwww</>)
 }
 // /https://github.com/CookPete/react-player/issues/699 IOS HLS fix but lmao fuck apple users fr fr
